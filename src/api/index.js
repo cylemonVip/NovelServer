8ebb286a24a45f897e8086185fd891f6d4513ef6
Sean <cylemonvip@163.com>
2018-10-24 22:40:29 +0800

    修复语法错误

--- src/api/index.js
+++ src/api/index.js
@@ -21,7 +21,7 @@ export const book = {
   // 章节内容:id 单章id
   chapterContent: 'http://chapter2.zhuishushenqi.com/chapter', //id: chapter id
   // 书籍搜索 可以搜索作者但是不精确
-  bookSearch: 'http://api.zhuishushenqi.com/book/fuzzy-search' //query ?query=凡人修仙传
+  bookSearch: 'http://api.zhuishushenqi.com/book/fuzzy-search', //query ?query=凡人修仙传
   //通过tag找列表
   bookSearchByTag: 'http://api.zhuishushenqi.com/book/by-tags' //query ?query={tags, start, limit}
 }
